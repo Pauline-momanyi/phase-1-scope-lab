8cd44ac2aaf1ce7dfba75000d633de10a197fee8
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-08 00:13:53 +0300

    demonstrate how functions determine the scope

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+//console.log(window.customerName);
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    //window.customerName
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "Sarah"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Norah"
+}
